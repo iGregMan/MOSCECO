e16b90f9a802d89ab3fac202cd740935b169c1c8
iGregMan <gregoire.maniel@protonmail.com>
2022-01-11 11:13:04 +0100

    API_IUCN Mise en page de départ

---
+++ API_IUCN.R
@@ -0,0 +1,27 @@
+##########################################################
+## MOSCECO: Modélisation, Scénarisation et Connectivité ##
+## de la biodiversité benthique des Antilles Françaises ##
+##########################################################
+
+# MANIEL     Grégoire    gregoire.maniel4@mnhn.fr
+# CORBARI    Laure
+# DIRBERG    Guillaume
+# FABRI-RUIZ Salomé
+# GOBERVILLE Eric
+# LEPAREUR   Fanny
+
+# Titre : Importation des données IUCN
+# Objectifs : Utiliser un API pour soumettre une recherche
+# directement au site de l'IUCN/
+
+# dossier de travail
+getwd()
+
+# librairies
+library(tidyverse)
+
+# fonctions
+
+# données
+
+# Traitement
